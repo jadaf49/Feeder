5f55ae05811beec05515de579075c7d5ba51d0ed
jadaf49 <jadaf49@nycstudents.net>
2024-09-18 03:14:30 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Feeder f = new Feeder();
+        System.out.println(f);
+        //System.out.println(f.currentFood);
+        System.out.println(f.getCurrentFood());
+        Feeder g = new Feeder(500);
+        System.out.println(g.getCurrentFood());
+        g.simulateOneDay(12);
+        System.out.println(Math.random());
+
+        int random = (int)(Math.random() * 10 + 1);
+        System.out.println(random);
+
+        System.out.println(Math.random() < 0.05);
+
+
+
+
+    }
+}
